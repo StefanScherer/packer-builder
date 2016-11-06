10c231f034fe810f08be357568bd3172bf003825
Stefan Scherer <scherer_stefan@icloud.com>
2016-11-06 02:22:56 +0100

    Remove suffix from parameter

--- upload.sh
+++ upload.sh
@@ -4,12 +4,12 @@ FILE=$2
 
 if [ -z "${NAME}" ] || [ "${NAME}" == "--help" ] || [ -z "${FILE}" ]; then
   echo "Usage: $0 machine jobname"
-  echo "$0 p1 windows_2016_docker_vmware"
+  echo "$0 p1 windows_2016_docker"
   exit 1
 fi
 
 today=$(date +%Y-%m-%d)
 cat <<CMD | ssh root@$(./packet.sh ip $NAME)
 $(pass azure-vagrantboxes)
-azure storage blob upload packer-windows/${FILE}.box box ${FILE}/$today/${FILE}.box
+azure storage blob upload packer-windows/${FILE}_vmware.box box ${FILE}/$today/${FILE}_vmware.box
 CMD
