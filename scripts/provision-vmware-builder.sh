4bf296d5f8e3d454344dc2dff98a588df656f857
Stefan Scherer <scherer_stefan@icloud.com>
2018-02-17 11:51:20 +0100

    Try Circle build

--- scripts/provision-vmware-builder.sh
+++ scripts/provision-vmware-builder.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-PACKER_VERSION=1.1.1
-VMWARE_VERSION=14.0.0-6661328
+PACKER_VERSION=1.1.3
+VMWARE_VERSION=14.1.1-7528167
+
+PACKER_URL=https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip
+VMWARE_URL=http://download3.vmware.com/software/wkst/file/VMware-Workstation-Full-${VMWARE_VERSION}.x86_64.bundle
 
 apt-get update
 apt-get install -qq git unzip curl
@@ -10,7 +13,7 @@ apt-get install -qq git unzip curl
 mkdir /opt/packer
 pushd /opt/packer
 echo "Downloading packer ${PACKER_VERSION} ...."
-curl -L -o ${PACKER_VERSION}_linux_amd64.zip https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip
+curl -L -o ${PACKER_VERSION}_linux_amd64.zip ${PACKER_URL}
 echo "Installing packer ${PACKER_VERSION} ..."
 unzip ${PACKER_VERSION}_linux_amd64.zip
 rm ${PACKER_VERSION}_linux_amd64.zip
@@ -25,7 +28,7 @@ apt-get install -qq x11-common x11-xserver-utils libxtst6 libxinerama1
 rmmod kvm_intel kvm
 
 echo "Downloading VMware Workstation ${VMWARE_VERSION} ..."
-curl -o VMware-Workstation.bundle https://download3.vmware.com/software/wkst/file/VMware-Workstation-Full-${VMWARE_VERSION}.x86_64.bundle
+curl -o VMware-Workstation.bundle ${VMWARE_URL}
 echo "Installing VMware Workstation ${VMWARE_VERSION} ..."
 sh ./VMware-Workstation.bundle --console --required --eulas-agreed
 rm ./VMware-Workstation.bundle
