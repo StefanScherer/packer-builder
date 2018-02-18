5d482d0d02a61adf2dc258a13ba9846e44ea97e8
Stefan Scherer <scherer_stefan@icloud.com>
2018-02-18 07:39:31 +0100

    Run packer-build in background

--- build.sh
+++ build.sh
@@ -17,4 +17,8 @@ echo "See the VNC port number and password in packer output."
 echo ""
 echo "ssh -L 127.0.0.1:5900:127.0.0.1:59xx root@$ip tail -f packer-windows/packer-build.log"
 
-ssh root@$(./packet.sh ip $NAME) ./packer-build.sh $FILE $HYPERVISOR
+ssh -n -f root@$(./packet.sh ip $NAME) "sh -c 'nohup ./packer-build.sh $FILE $HYPERVISOR > /dev/null 2>&1 &'"
+
+sleep 20
+
+ssh root@$(./packet.sh ip $NAME) tail -f packer-windows/packer-build.log
