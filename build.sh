ff4bc5eb83bb816c07a06f1b865bafc9dce50a93
Stefan Scherer <scherer_stefan@icloud.com>
2018-02-18 20:44:18 +0100

    Only stop

--- build.sh
+++ build.sh
@@ -44,7 +44,7 @@ else
   if [ -e ${FILE}_virtualbox.box ]; then
     azure storage blob upload ${FILE}_virtualbox.box vagrantbox ${FILE}/$today/${FILE}_virtualbox.box
   fi
-  packet.sh delete \$(hostname)
+  packet.sh stop \$(hostname)
 CMD
   chmod +x packer-upload-and-destroy.sh
   scp packer-upload-and-destroy.sh root@$ip:
