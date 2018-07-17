f05ad906bcad171baf86ac716d05da5b98a8b0af
Stefan Scherer <scherer_stefan@icloud.com>
2018-07-17 21:40:49 +0200

    Fix service principal scope

--- hyperv/init_azure.sh
+++ hyperv/init_azure.sh
@@ -13,7 +13,7 @@ location=$(grep -A1 location variables.tf | grep -v location | awk '{gsub(/"/, "
 resource_group=$(grep -A1 resource_group variables.tf | grep -v resource_group | awk '{gsub(/"/, "", $3); print $3}')
 
 echo "Creating resource group $resource_group"
-scope=$(az group create -n $resource_group -l "$location" | jq -r .id)
+scope=$(az group create -n "$resource_group" -l "$location" | jq -r .id)
 
 aadClientName="Terraform-$resource_group"
 ARM_CLIENT_SECRET=$(pwgen 30 1)
@@ -24,16 +24,14 @@ ARM_SUBSCRIPTION_ID=$(az account show | jq -r .id)
 ARM_TENANT_ID=$(az account show | jq -r .tenantId)
 
 set +e
-
-if az ad sp show --id "http://$aadClientName" 2>/dev/null; then
-  set -e
-  ARM_CLIENT_ID=$(az ad sp credential reset --name $aadClientName --password $ARM_CLIENT_SECRET | jq -r .appId)
-else
-  set -e
-  ARM_CLIENT_ID=$(az ad sp create-for-rbac -n $aadClientName --password $ARM_CLIENT_SECRET --role contributor --scopes "$scope" | jq -r .appId)
+aadClientId=$(az ad sp show --id "http://$aadClientName" | jq -r .id)
+if [ "$aadClientId" != "" ]; then
+  echo "Deleting old service principal $aadClientName"
+  az ad sp delete --id "http://$aadClientName"
 fi
+set -e
+ARM_CLIENT_ID=$(az ad sp create-for-rbac -n "$aadClientName" --password "$ARM_CLIENT_SECRET" --role contributor --scopes "$scope" | jq -r .appId)
 
-org=$giturl
 org=$(git remote get-url origin | sed 's/.*github.com://' | sed 's/.*github.com\///' | sed 's/\.git$//' | sed 's/\/.*$//' )
 repo=$(git remote get-url origin | sed 's/.*github.com://' | sed 's/.*github.com\///' | sed 's/\.git$//' | sed 's/.*\///' )
 
@@ -47,7 +45,7 @@ ARM_TENANT_ID
 echo "Deleting CircleCI environment variables for $org/$repo"
 for i in $circlevars
 do
-  curl -X DELETE -sS --fail --header "Content-Type: application/json" "https://circleci.com/api/v1.1/project/github/$org/$repo/envvar/$i?circle-token=$CIRCLECI_TOKEN"  >/dev/null
+  curl -X DELETE -sS --fail --header "Content-Type: application/json" "https://circleci.com/api/v1.1/project/github/$org/$repo/envvar/$i?circle-token=$CIRCLECI_TOKEN"  >/dev/null || true
 done
 
 echo "Setting CircleCI environment variables for $org/$repo"
