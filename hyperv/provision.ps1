667ae968cb7e5cde186a2ee8c965c5fbbc7e29b9
Stefan Scherer <scherer_stefan@icloud.com>
2018-12-24 00:00:51 +0100

    Put Git unix tools into path

--- hyperv/provision.ps1
+++ hyperv/provision.ps1
@@ -13,11 +13,12 @@ Function SetupPhase1 {
   Write-Host "Installing Chocolatey"
   iex (wget 'https://chocolatey.org/install.ps1' -UseBasicParsing)
   choco feature disable --name showDownloadProgress
-  choco install -y git
+  choco install -y git /GitAndUnixToolsOnPath
+  rm -force 'C:\Program Files\Git\usr\bin\ssh*'
   choco install -y curl
   choco install -y packer -version 1.3.3.20181213
   # choco install -y vagrant -version 2.0.3
-  choco install -y terraform -version 0.11.11
+  choco install -y terraform -version 0.11.9
   choco install -y nodejs -version 8.10.0
   choco install -y procexp
   choco install -y procmon
