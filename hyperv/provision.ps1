7a642d1350789cbf9ac3fe7b154542e802ebd484
Stefan Scherer <scherer_stefan@icloud.com>
2019-01-10 07:32:05 +0100

    Use Write-Output

--- hyperv/provision.ps1
+++ hyperv/provision.ps1
@@ -10,7 +10,7 @@ Function SetupPhase1 {
 
   New-ItemProperty -Path HKCU:\Software\Microsoft\ServerManager -Name DoNotOpenServerManagerAtLogon -PropertyType DWORD -Value "1" -Force
 
-  Write-Host "Installing Chocolatey"
+  Write-Output "Installing Chocolatey"
   iex (curl.exe https://chocolatey.org/install.ps1)
   choco feature disable --name showDownloadProgress
   choco install -y git
@@ -23,64 +23,64 @@ Function SetupPhase1 {
   choco install -y procmon
   choco install -y azure-cli
 
-  Write-Host "Installing Hyper-V"
+  Write-Output "Installing Hyper-V"
   Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart
   Install-WindowsFeature Hyper-V-Tools
   Install-WindowsFeature Hyper-V-PowerShell
 
-#Write-Host Install all Windows Updates
+#Write-Output Install all Windows Updates
 #Get-Content C:\windows\system32\en-us\WUA_SearchDownloadInstall.vbs | ForEach-Object {
 #  $_ -replace 'confirm = msgbox.*$', 'confirm = vbNo'
 #} | Out-File $env:TEMP\WUA_SearchDownloadInstall.vbs
 #"a`na" | cscript $env:TEMP\WUA_SearchDownloadInstall.vbs
 
-  Write-Host "Rebooting"
+  Write-Output "Rebooting"
   Restart-Computer
 }
 
 Function SetupPhase2 {
 
-  # Write-Host "Installing Vagrant plugins"
+  # Write-Output "Installing Vagrant plugins"
   # vagrant plugin install vagrant-reload
 
-  Write-Host "Intalling azure-cli"
+  Write-Output "Intalling azure-cli"
   npm install -g azure-cli
 
-  Write-Host "Adding NAT"
+  Write-Output "Adding NAT"
   New-VMSwitch -SwitchName "packer-hyperv-iso" -SwitchType Internal
   New-NetIPAddress -IPAddress 192.168.0.1 -PrefixLength 24 -InterfaceIndex (Get-NetAdapter -name "vEthernet (packer-hyperv-iso)").ifIndex
   New-NetNat -Name MyNATnetwork -InternalIPInterfaceAddressPrefix 192.168.0.0/24
 
-  Write-Host "Adding DHCP scope"
+  Write-Output "Adding DHCP scope"
   Install-WindowsFeature DHCP -IncludeManagementTools
   Add-DhcpServerv4Scope -Name "Internal" -StartRange 192.168.0.10 -EndRange 192.168.0.250 -SubnetMask 255.255.255.0 -Description "Internal Network"
   Set-DhcpServerv4OptionValue -ScopeID 192.168.0 -DNSServer 8.8.8.8 -Router 192.168.0.1
 
-  Write-Host "Allow Packer http server"
+  Write-Output "Allow Packer http server"
   New-NetFirewallRule -DisplayName "Allow Packer" -Direction Inbound -Program "C:\ProgramData\chocolatey\lib\packer\tools\packer.exe" -RemoteAddress LocalSubnet -Action Allow
 
-  Write-Host "Disabling autologon"
+  Write-Output "Disabling autologon"
   New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoAdminLogon -PropertyType DWORD -Value "0" -Force
 
-  Write-Host "Downloading OpenSSH"
+  Write-Output "Downloading OpenSSH"
   curl.exe -o OpenSSH-Win64.zip -L https://github.com/PowerShell/Win32-OpenSSH/releases/download/v7.7.2.0p1-Beta/OpenSSH-Win64.zip
   
-  Write-Host "Expanding OpenSSH"
+  Write-Output "Expanding OpenSSH"
   Expand-Archive OpenSSH-Win64.zip C:\
   Remove-Item -Force OpenSSH-Win64.zip
 
   Push-Location C:\OpenSSH-Win64
 
-  Write-Host "Installing OpenSSH"
+  Write-Output "Installing OpenSSH"
   & .\install-sshd.ps1
 
-  Write-Host "Generating host keys"
+  Write-Output "Generating host keys"
   .\ssh-keygen.exe -A
 
-  Write-Host "Fixing host file permissions"
+  Write-Output "Fixing host file permissions"
   & .\FixHostFilePermissions.ps1 -Confirm:$false
 
-  Write-Host "Fixing user file permissions"
+  Write-Output "Fixing user file permissions"
   & .\FixUserFilePermissions.ps1 -Confirm:$false
 
   Pop-Location
@@ -88,28 +88,28 @@ Function SetupPhase2 {
   $newPath = 'C:\OpenSSH-Win64;' + [Environment]::GetEnvironmentVariable("PATH", [EnvironmentVariableTarget]::Machine)
   [Environment]::SetEnvironmentVariable("PATH", $newPath, [EnvironmentVariableTarget]::Machine)
 
-  Write-Host "Adding public key to authorized_keys"
+  Write-Output "Adding public key to authorized_keys"
   $keyPath = "~\.ssh\authorized_keys"
   New-Item -Type Directory ~\.ssh > $null
   $sshKey | Out-File $keyPath -Encoding Ascii
 
-  Write-Host "Opening firewall port 22"
+  Write-Output "Opening firewall port 22"
   New-NetFirewallRule -Protocol TCP -LocalPort 22 -Direction Inbound -Action Allow -DisplayName SSH
 
-  Write-Host "Setting sshd service startup type to 'Automatic'"
+  Write-Output "Setting sshd service startup type to 'Automatic'"
   Set-Service sshd -StartupType Automatic
   Set-Service ssh-agent -StartupType Automatic
-  Write-Host "Setting sshd service restart behavior"
+  Write-Output "Setting sshd service restart behavior"
   sc.exe failure sshd reset= 86400 actions= restart/500
 
-  Write-Host "Configuring sshd"
+  Write-Output "Configuring sshd"
   (Get-Content C:\ProgramData\ssh\sshd_config).replace('#TCPKeepAlive yes', 'TCPKeepAlive yes').replace('#ClientAliveInterval 0', 'ClientAliveInterval 300').replace('#ClientAliveCountMax 3', 'ClientAliveCountMax 3') | Set-Content C:\ProgramData\ssh\sshd_config
 
-  Write-Host "Starting sshd service"
+  Write-Output "Starting sshd service"
   Start-Service sshd
   Start-Service ssh-agent
   
-  Write-Host "Removing scheduled job"
+  Write-Output "Removing scheduled job"
   Unregister-ScheduledJob -Name NewServerSetupResume -Force
 }
 
