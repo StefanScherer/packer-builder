f90f5c8a1aa8784c31faffea1b61f8cde340e22e
Stefan Scherer <scherer_stefan@icloud.com>
2020-02-02 20:17:07 +0100

    Rollback to VMware 14.1.7

--- scripts/provision-virtualbox+vmware-builder.sh
+++ scripts/provision-virtualbox+vmware-builder.sh
@@ -4,7 +4,7 @@ set -x
 echo "Running provision-virtualbox+vmware-builder.sh"
 
 PACKER_VERSION=1.5.1
-VMWARE_VERSION=14.1.8-14921873
+VMWARE_VERSION=14.1.7-12989993
 VIRTUALBOX_VERSION=6.1
 
 PACKER_URL=https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip
